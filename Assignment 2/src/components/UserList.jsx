792156705ed63b4493f7b892d22d7948cb939ab1
MD-Kayesur <rmdkayesur@gmail.com>
2025-08-30 02:17:53 +0600

    assignment 2

---
+++ Assignment 2/src/components/UserList.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export class UserList extends Component{
+    render(){
+        return(
+            <>
+            
+            UserList
+            </>
+        )
+    }
+}
